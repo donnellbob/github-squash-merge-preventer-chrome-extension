6f8adb190700bd0ae55765ae8b223e678d097e1f
Patrick <patrick.donnell13@gmail.com>
2024-05-13 17:00:07 +0930

    Update text and selection method, default to disable develop

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -1,18 +1,22 @@
-function checkBranch(){
-    chrome.storage.sync.get(({branchesList:[]}), (data)=>{  
-        const targetBranch = document.querySelector("span.commit-ref span.css-truncate-target").innerText
-        if (data.branchesList.includes(targetBranch)){
-            const button = document.querySelector("button.js-merge-box-button-squash")
-            if (button){
-                button.disabled=true
-            }else{
-                setTimeout(checkBranch,125)
-            }
-        }    
-    })
+function checkBranch() {
+  chrome.storage.sync.get({ branchesList: [] }, (data) => {
+    const targetBranch = document.querySelector(
+      "span.head-ref span.css-truncate-target"
+    ).innerText;
+    if (data.branchesList.includes(targetBranch)) {
+      const buttons = document.getElementsByClassName("btn-group-squash");
+      for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+        buttons[i].textContent = "DO NOT SQUASH DEVELOP MERGES";
+      }
+
+      if (buttons.length === 0) {
+        setTimeout(checkBranch, 125);
+      }
+    }
+  });
 }
 
-
-window.onload = function() {
-    setTimeout(checkBranch, 125)
-}
+window.onload = function () {
+  setTimeout(checkBranch, 125);
+};
