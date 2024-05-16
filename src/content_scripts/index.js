839d0afd5425317cbb9fcd086202bb6bd4e253ba
Patrick <patrick.donnell13@gmail.com>
2024-05-16 13:22:55 +0930

    add infinite loop to get all use cases

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -2,18 +2,15 @@ function checkBranch() {
   chrome.storage.sync.get({ branchesList: ["develop"] }, (data) => {
     const targetBranch = document.querySelector(
       "span.head-ref span.css-truncate-target"
-    ).innerText;
+    )?.innerText;
     if (data.branchesList.includes(targetBranch)) {
       const buttons = document.getElementsByClassName("btn-group-squash");
       for (let i = 0; i < buttons.length; i++) {
         buttons[i].disabled = true;
         buttons[i].textContent = "DO NOT SQUASH DEVELOP MERGES";
       }
-
-      if (buttons.length === 0) {
-        setTimeout(checkBranch, 125);
-      }
     }
+    setTimeout(checkBranch, 1250);
   });
 }
 
