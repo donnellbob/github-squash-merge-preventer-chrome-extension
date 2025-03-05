eb26fe8cd57de793eb2303a962628d0d7c0e73a1
Patrick <patrick.donnell13@gmail.com>
2025-03-05 13:19:05 +1030

    support new merge flow

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -4,6 +4,15 @@ function checkBranch() {
       "span.head-ref span.css-truncate-target"
     )?.innerText;
     if (data.branchesList.includes(targetBranch)) {
+      // New merge flow
+      for (const btn of document.querySelectorAll("button")) {
+        if (btn.innerText.includes("Squash and merge")) {
+          btn.disabled = true;
+          btn.textContent = "DO NOT SQUASH DEVELOP MERGES";
+        }
+      }
+
+      // Legacy merge flow
       const buttons = document.getElementsByClassName("btn-group-squash");
       for (let i = 0; i < buttons.length; i++) {
         buttons[i].disabled = true;
